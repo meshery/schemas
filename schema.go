0a288cda8e73c301d303a86f810386fe53632768
Shubham Upadhyay <58693304+shubham251972@users.noreply.github.com>
2024-07-16 22:55:42 +0000

    Update schema go package script
    Signed-off-by: Shubham Upadhyay <58693304+shubham251972@users.noreply.github.com>

--- schema.go
+++ schema.go
@@ -1,7 +1,28 @@
 package schemas
 
-import "embed"
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+)
 
-//go:embed schemas/openapi.yml
-
+// Embedding the file system for schema files
+//go:embed schemas
 var Schemas embed.FS
+
+// LoadSchema loads and parses the JSON schema file from the embedded filesystem.
+func LoadSchema(fileName string) (map[string]interface{}, error) {
+	// Read the file from the embedded filesystem
+	data, err := Schemas.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	// Unmarshal JSON data into a map
+	var schema map[string]interface{}
+	if err := json.Unmarshal(data, &schema); err != nil {
+		return nil, fmt.Errorf("error unmarshalling JSON: %w", err)
+	}
+
+	return schema, nil
+}
