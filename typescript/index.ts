169247d78d0fe959e95287aa6c89a777b5cee151
codeSafari10 <codeSafari10@gmail.com>
2024-07-26 11:40:40 +0530

    generate openapi types
    Signed-off-by: codeSafari10 <codeSafari10@gmail.com>

--- typescript/index.ts
+++ typescript/index.ts
@@ -11,10 +11,26 @@ import {
 import { DesignSchema } from "./constructs/v1alpha2/design";
 import { HttpsSchemasMesheryIoRelationshipJson } from "./constructs/v1alpha2/relationship";
 
+// v1alpha3
+import {HttpsSchemasMesheryIoRelationshipJson  as V1alpha3Relationship} from "./constructs/v1alpha3/relationship"
+import { 
+  HttpsSchemasMesheryIoSelectorsJson as V1alpha3Selectors
+} from "./constructs/v1alpha3/selectors"
+
 // v1beta1
 import { HttpsSchemasMesheryIoComponentJson } from "./constructs/v1beta1/component";
 import { HttpsSchemasMesheryIoModelJson } from "./constructs/v1beta1/model";
+import {DesignSchema as V1beta1DesignSchema} from "./constructs/v1beta1/designs"
 
+// core
+import {CoreJson} from "./constructs/core"
+
+//OpenAPI
+import {components,paths,webhooks} from "./openapi"
+
+export type Core = CoreJson;
+
+// Contructs 
 export namespace v1alpha1 {
   export type CatalogData = CatalogDataT;
   export type CatalogCaveatsAndConsiderations = CaveatsAndConsiderations;
@@ -25,7 +41,20 @@ export namespace v1alpha2 {
   export type Relationship = HttpsSchemasMesheryIoRelationshipJson;
 }
 
+export namespace v1alpha3 {
+  export type Relationship = V1alpha3Relationship;
+  export type Selectors = V1alpha3Selectors;
+}
+
 export namespace v1beta1 {
   export type Component = HttpsSchemasMesheryIoComponentJson;
   export type Model = HttpsSchemasMesheryIoModelJson;
+  export type Design = V1beta1DesignSchema;
+}
+
+// OpenAPI
+export namespace api {
+  export type Components = components;
+  export type Paths = paths;
+  export type Webhooks = webhooks;
 }
