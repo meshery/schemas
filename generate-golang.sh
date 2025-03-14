abf53037a4ac8517af1d11f3126a4b8a5926ad67
aabidsofi19 <mailtoaabid01@gmail.com>
2025-03-14 15:49:11 +0530

    add support for specifying filepath
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

--- generate-golang.sh
+++ generate-golang.sh
@@ -4,28 +4,36 @@
 generate_schema_models() {
     local package="$1"
     local version="$2"
+    local input_overide="$3"
 
     if [[ -z "$package" || -z "$version" ]]; then
         echo "Usage: generate_api_models <package> <version>"
         return 1
     fi
 
-    local input_schema="schemas/constructs/${version}/${package}/openapi.yml"
+    echo "Generating Go models for package: $package, version: $version"
+
+    if [[ -z "${input_overide}" ]]; then
+        local input_schema="schemas/constructs/${version}/${package}/openapi.yml"
+    else
+        local input_schema="${input_overide}"
+    fi
+
+    # local input_schema="schemas/constructs/${version}/${package}/openapi.yml"
     local merged_output="schemas/constructs/${version}/${package}/merged-openapis.yml"
 
-    local merged_output2="schemas/constructs/${version}/${package}/openapi.yml"
+    # local merged_output2="schemas/constructs/${version}/${package}/openapi.yml"
     local output_go_file="models/${version}/${package}/${package}.go"
-
     echo "Bundling OpenAPI schema..."
-    npx swagger-cli bundle -r "$input_schema" -o "$merged_output"
-
+    npx swagger-cli bundle --dereference "$input_schema" -o "$merged_output"
     if [[ $? -ne 0 ]]; then
         echo "Error: Failed to bundle OpenAPI schema."
         return 1
     fi
 
     echo "Generating Go models..."
-    oapi-codegen --config openapi.config.yml   --package "$package" -generate types --include-tags all -o "$output_go_file" "$merged_output2"
+    oapi-codegen --config openapi.config.yml    --package "$package" -generate types --include-tags all -o "$output_go_file" "$merged_output"
+    rm -f "$merged_output"
 
     if [[ $? -ne 0 ]]; then
         echo "Error: Failed to generate Go models."
@@ -36,5 +44,9 @@ generate_schema_models() {
 }
 
 
-generate_schema_models "model" "v1beta1"
+# generate_schema_models "model" "v1beta1"
 generate_schema_models "capability" "v1alpha1"
+generate_schema_models "category" "v1beta1"
+generate_schema_models "pattern" "v1beta1" "schemas/constructs/v1beta1/design/openapi.yml"
+generate_schema_models "core" "v1alpha1"
+generate_schema_models "catalog" "v1alpha2"
