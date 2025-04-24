3c0d997f214f52b232dbeb428d603c2beff7b509
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-04-24 17:51:46 +0530

    use comman js
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["typescript/index.ts"],
-  format: ["cjs", "esm"],
+  format: ["cjs"],
   dts: true,
   splitting: false,
   sourcemap: false,
