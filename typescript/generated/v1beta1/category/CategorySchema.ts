1ff4f45f320fe28a03db32d91a37bdab3ddebf06
aabidsofi19 <aabidsofi19@users.noreply.github.com>
2026-01-09 18:54:13 +0000

    Generate build artifacts from schemas
    Signed-off-by: meshery <ci@meshery.io>

---
+++ typescript/generated/v1beta1/category/CategorySchema.ts
@@ -0,0 +1,81 @@
+/**
+ * This file was automatically generated from OpenAPI schema.
+ * Do not manually modify this file.
+ */
+
+const CategorySchema = {
+  "openapi": "3.0.0",
+  "info": {
+    "title": "category",
+    "version": "1.0.0"
+  },
+  "paths": {},
+  "components": {
+    "schemas": {
+      "CategoryDefinition": {
+        "$id": "https://schemas.meshery.io/category.json",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "description": "Category of the model.",
+        "required": [
+          "id",
+          "name",
+          "metadata"
+        ],
+        "properties": {
+          "id": {
+            "x-order": 1,
+            "type": "string",
+            "format": "uuid",
+            "description": "A Universally Unique Identifier used to uniquely identify entities in Meshery. The UUID core definition is used across different schemas.",
+            "x-go-type": "uuid.UUID",
+            "x-go-type-import": {
+              "path": "github.com/gofrs/uuid"
+            }
+          },
+          "name": {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 100,
+            "x-oapi-codegen-extra-tags": {
+              "yaml": "name",
+              "json": "name",
+              "gorm": "name"
+            },
+            "default": "Uncategorized",
+            "description": "The category of the model that determines the main grouping.",
+            "enum": [
+              "Analytics",
+              "App Definition and Development",
+              "Cloud Native Network",
+              "Cloud Native Storage",
+              "Database",
+              "Machine Learning",
+              "Observability and Analysis",
+              "Orchestration & Management",
+              "Platform",
+              "Provisioning",
+              "Runtime",
+              "Security & Compliance",
+              "Serverless",
+              "Tools",
+              "Uncategorized"
+            ],
+            "x-order": 2
+          },
+          "metadata": {
+            "type": "object",
+            "x-oapi-codegen-extra-tags": {
+              "yaml": "metadata,omitempty",
+              "json": "metadata,omitempty",
+              "gorm": "type:bytes;serializer:json"
+            },
+            "x-order": 3
+          }
+        }
+      }
+    }
+  }
+} as const;
+
+export default CategorySchema;
